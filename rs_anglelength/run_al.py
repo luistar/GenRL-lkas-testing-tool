afe6d9ec280de931475327ac0c833cfc5a4161c0
Anna Scognamiglio <annascognamiglio2503@gmail.com>
2022-02-19 18:57:17 +0100

    Refactoring

--- rs_anglelength/run_al.py
+++ rs_anglelength/run_al.py
@@ -10,6 +10,7 @@ from code_pipeline.executors import MockExecutor
 from code_pipeline.visualization import RoadTestVisualizer
 from scipy.interpolate import make_interp_spline, interp1d
 
+
 class AngleLengthGenerator:
 
     def __init__(self, executor: None, map_size: None):
@@ -72,91 +73,6 @@ class AngleLengthGenerator:
             )
         return road_points
 
-    def get_road_pointsP(self, array):
-        road_points = []
-        road_points.append((420, 180))
-        road_points.append((400, 180))
-        road_points.append((350, 180))
-        road_points.append((300, 180))
-        for i in range(len(array)):
-            road_points.append(
-                (
-                    array[i][0], #* self.map_size,
-                    array[i][1]# * self.map_size
-                )
-            )
-        return road_points
-
-    def get_road_pointsW(self):
-        road_points = []
-        road_points.append((150, 180))
-        road_points.append((60, 180))
-        road_points.append((128, 365))
-        return road_points
-
-    def prova(self):
-        road_points = []
-        road_points.append((52, 23.637500047683716))
-        road_points.append((53, 54.637500047683716))
-        road_points.append((53, 109.63750004768372))
-        road_points.append((56, 160.63750004768372))
-        road_points.append((55, 214.63750004768372))
-        road_points.append((60, 269.6375000476837))
-        road_points.append((57, 314.6375000476837))
-        road_points.append((60, 377.6375000476837))
-        road_points.append((75, 425.6375000476837))
-        road_points.append((104, 454.6375000476837))
-        road_points.append((176, 463.6375000476837))
-        road_points.append((200, 461.6375000476837))
-        road_points.append((253, 443.6375000476837))
-        #road_points.append((282, 420.6375000476837))
-        #road_points.append((278, 389.6375000476837))
-        road_points.append((245, 355.6375000476837))
-        road_points.append((212, 340.6375000476837))
-        road_points.append((194, 327.6375000476837))
-        road_points.append((202, 286.6375000476837))
-        road_points.append((230, 275.6375000476837))
-        road_points.append((255, 271.6375000476837))
-        road_points.append((280, 256.6375000476837))
-        road_points.append((300, 207.63750004768372))
-        road_points.append((273, 187.63750004768372))
-        road_points.append((244, 175.63750004768372))
-        road_points.append((223, 172.63750004768372))
-        return road_points
-
-    def prova2(self):
-        road_points = []
-
-        road_points.append((194, 30.4375))
-        road_points.append((206, 51.4375))
-        road_points.append((212, 73.4375))
-        road_points.append((219, 110.4375))
-        road_points.append((210, 135.4375))
-        road_points.append((194, 161.4375))
-        road_points.append((179, 184.4375))
-        road_points.append((151, 215.4375))
-        road_points.append((140, 258.4375))
-        road_points.append((164, 283.4375))
-        road_points.append((183, 297.4375))
-        road_points.append((200, 303.4375))
-        road_points.append((216, 326.4375))
-        road_points.append((227, 343.4375))
-        road_points.append((228, 362.4375))
-        road_points.append((212, 382.4375))
-        road_points.append((195, 388.4375))
-        road_points.append((168, 393.4375))
-        road_points.append((140, 402.4375))
-        road_points.append((136, 419.4375))
-        road_points.append((144, 438.4375))
-        road_points.append((158, 456.4375))
-        road_points.append((185, 471.4375))
-        road_points.append((211, 482.4375))
-        # road_points.append((154, 431.4375))
-        # road_points.append((161, 450.4375))
-        # road_points.append((185, 463.4375))
-        # road_points.append((215, 468.4375))
-        return road_points
-
     def check_boundaries(self, x, y):
         if int(x) < self.min_coordinate or int(x) > self.max_coordinate or int(y) < self.min_coordinate or int(y) > self.max_coordinate:
             return False
@@ -208,28 +124,22 @@ class AngleLengthGenerator:
                 done = True
         return arr
 
-    def start(self):
+    def get_max_oob_percentage(self, execution_data):
+        max_oob_percentage = 0
+        for record in execution_data[1]:
+            logging.info(f"Processing record with oob: {record.oob_percentage}")
+            if record.oob_percentage > max_oob_percentage:
+                logging.debug(f"New oob max: {record.oob_percentage}")
+                max_oob_percentage = record.oob_percentage
+        #logging.debug(f"Returning oob max: {max_oob_percentage}")
+        return max_oob_percentage
 
-        #test_executor = MockExecutor(result_folder="results", time_budget=1e10, map_size=500, road_visualizer=RoadTestVisualizer(map_size=500))
-
-        test_executor = BeamngExecutor(generation_budget=10000, execution_budget=10000, time_budget=10000, result_folder="results", map_size=500, beamng_home="C:\\Users\\kikki\\BeamNG", beamng_user="C:\\Users\\kikki\\BeamNG_user", road_visualizer=RoadTestVisualizer(map_size=500))
-
-        (x, y) = (60, 180)#(random.uniform(self.min_coordinate, self.max_coordinate), random.uniform(self.min_coordinate, self.max_coordinate))
-        angle = 70#random.randint(45, 315) #meno di 45 o più di 315 -> the road is too sharp
-        length =200# random.randint(100, 200)
-        print(x, y, angle)
-        final = self.three_points(x, y, angle, length)
-        print(final)
-        # final = self.more_points(x, y, angle, length)
+    def check_final_and_test(self, final, test_executor):
         if final is None:
             print("Errore nella costruzione dei punti")
         else:
-            #road_points = self.get_road_points(final)
-            road_points = self.prova()
-            print(road_points)
-            #plt.scatter(final[:, 0], final[:, 1])
+            road_points = self.get_road_points(final)
             self.draw_roadpoints(road_points)
-            #self.draw_spline(final)
 
             the_test = RoadTestFactory.create_road_test(road_points)
             is_valid, validation_message = test_executor.validate_test(the_test)
@@ -242,8 +152,62 @@ class AngleLengthGenerator:
                     print("Test is valid and passed.")
                 elif test_outcome == "FAIL":
                     print("Test is valid and failed.")
+                return is_valid, execution_data
             else:
                 print(f"Test is invalid: {validation_message}")
+                return is_valid, validation_message
+
+    def check_oob(self):
+        with open('saveoob.txt', 'w') as f:
+            test_executor = BeamngExecutor(generation_budget=10000, execution_budget=10000, time_budget=10000,
+                                       result_folder="results", map_size=500, beamng_home="C:\\Users\\kikki\\BeamNG",
+                                       beamng_user="C:\\Users\\kikki\\BeamNG_user",
+                                       road_visualizer=RoadTestVisualizer(map_size=500))
+            (x, y) = (42, 42)
+            for a in range(45, 315, 10):
+                for l in range(50, 200, 10):
+                    final = self.three_points(x, y, a, l)
+                    flag, res = self.check_final_and_test(final, test_executor)
+                    if flag:
+                        moob = self.get_max_oob_percentage(res)
+                        f.write("Angolo: ")
+                        f.write(repr(a))
+                        f.write(" Lunghezza: ")
+                        f.write(repr(l))
+                        f.write(" Max oob: ")
+                        f.write(repr(moob))
+                        f.write('\n')
+                    else:
+                        f.write("Angolo: ")
+                        f.write(repr(a))
+                        f.write(" Lunghezza: ")
+                        f.write(repr(l))
+                        f.write(" ")
+                        f.write(repr(res))
+                        f.write('\n')
+            f.close()
+
+
+
+    # def start(self):
+    #
+    #     #test_executor = MockExecutor(result_folder="results", time_budget=1e10, map_size=500, road_visualizer=RoadTestVisualizer(map_size=500))
+    #
+    #     test_executor = BeamngExecutor(generation_budget=10000, execution_budget=10000, time_budget=10000, result_folder="results", map_size=500, beamng_home="C:\\Users\\kikki\\BeamNG", beamng_user="C:\\Users\\kikki\\BeamNG_user", road_visualizer=RoadTestVisualizer(map_size=500))
+    #
+    #     (x, y) = (60, 180)#(random.uniform(self.min_coordinate, self.max_coordinate), random.uniform(self.min_coordinate, self.max_coordinate))
+    #     angle = 70#random.randint(45, 315) #meno di 45 o più di 315 -> the road is too sharp
+    #     length =100# random.randint(100, 200)
+    #     print(x, y, angle)
+    #     final = self.three_points(x, y, angle, length)
+    #     print(final)
+    #     # final = self.more_points(x, y, angle, length)
+    #     execution_data = self.check_final_and_test(final, test_executor)
+    #     print(self.get_max_oob_percentage(execution_data))
+
+    def start(self):
+        self.check_oob()
+
 
 # a = AngleLengthGenerator("mock", 500)
 # # rp = a.get_road_points(a.build_road(10))
